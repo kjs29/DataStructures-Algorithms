d9fe31217e2ce88c74d548dae82d34eb272c663a
Jinsung <jsk.jinsung@gmail.com>
2023-07-24 12:29:24 -0600

    added til 7.22 merge sort array.md

--- Questions/leetcode.py
+++ Questions/leetcode.py
@@ -1,20 +1,23 @@
 leetcode_questions_solved = {}
-
 leetcode_questions_solved[27] = "Remove element"
 leetcode_questions_solved[26] = "Remove duplicates from sorted array"
 leetcode_questions_solved[1089] = "Duplicate Zeros"
+leetcode_questions_solved[88] = "Merge sorted Array"
+leetcode_questions_solved[283] = "Move Zeros"
+leetcode_questions_solved[167] = "Two Sum II - Input Array Is Sorted"
 
 
 leetcode_questions_not_solved = {}
-
 leetcode_questions_not_solved.update({859:"buddy strings"})
+leetcode_questions_not_solved.update({349:"Intersection of two arrays"})
+leetcode_questions_not_solved.update({15:"Three Sum"})
 
 
 leetcode_questions_requires_more_practice = {}
-
 leetcode_questions_requires_more_practice[27] = leetcode_questions_solved[27]
 leetcode_questions_requires_more_practice[26] = leetcode_questions_solved[26]
 leetcode_questions_requires_more_practice[1089] = leetcode_questions_solved[1089]
+leetcode_questions_requires_more_practice[88] = leetcode_questions_solved[88]
 
 print(f"\nleetcode_questions_solved : {leetcode_questions_solved}")
 print(f"\nleetcode_questions_not_solved : {leetcode_questions_not_solved}")
