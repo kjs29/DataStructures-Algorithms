7a6edc452591132162011467a573b4d3f99c3ea8
Jinsung <jsk.jinsung@gmail.com>
2023-08-02 15:54:21 -0600

    added til swap, shift, bubble sort

--- Questions/leetcode.py
+++ Questions/leetcode.py
@@ -6,12 +6,18 @@ leetcode_questions_solved[88] = "Merge sorted Array"
 leetcode_questions_solved[283] = "Move Zeros"
 leetcode_questions_solved[167] = "Two Sum II - Input Array Is Sorted"
 leetcode_questions_solved[707] = "Design Linked List"
+leetcode_questions_solved[876] = "Middle of the Linked List"
+leetcode_questions_solved[141] = "Linked List Cycle"
+leetcode_questions_solved[206] = "Reverse Linked List"
 
 
 leetcode_questions_not_solved = {}
 leetcode_questions_not_solved.update({859:"buddy strings"})
 leetcode_questions_not_solved.update({349:"Intersection of two arrays"})
 leetcode_questions_not_solved.update({15:"Three Sum"})
+leetcode_questions_not_solved.update({1290:"Convert Binary Number in a Linked List to Integer"})
+leetcode_questions_not_solved.update({21:"Merge Two Sorted Lists"})
+
 
 
 leetcode_questions_requires_more_practice = {}
@@ -20,6 +26,7 @@ leetcode_questions_requires_more_practice[26] = leetcode_questions_solved[26]
 leetcode_questions_requires_more_practice[1089] = leetcode_questions_solved[1089]
 leetcode_questions_requires_more_practice[88] = leetcode_questions_solved[88]
 leetcode_questions_requires_more_practice[707] = leetcode_questions_solved[707]
+leetcode_questions_requires_more_practice[876] = leetcode_questions_solved[876]
 
 print(f"\nleetcode_questions_solved : {leetcode_questions_solved}")
 print(f"\nleetcode_questions_not_solved : {leetcode_questions_not_solved}")
--- README.md
+++ README.md
@@ -26,10 +26,44 @@
 
 # Array 
 
-- Accessing: O(1)
+- Accessing an element: O(1) - Good!
 
-- Searching: O(n) - linear search
+- Searching for an element:
 
-- Inserting: O(n)
+    - Linear Search: O(n)
 
-- Deleting: O(n)
+- Inserting an element:
+
+    - Inserting at the end: O(1)
+
+    - anywhere else: O(n)
+
+- Deleting an element: 
+
+    - Deleting at the end: O(1)
+
+    - anywhere else: O(n)
+
+# LinkedList (Singly)
+
+- Accessing: 
+    
+    - head: O(1)
+
+    - other nodes: O(n) because it needs to traverse the list, which takes O(n)
+
+- Searching for a node:
+
+    - Linear Search: O(n)
+
+- Inserting an element (Inserting itself is O(1)):
+
+    - Inserting at the front: O(1)
+
+    - anywhere else: O(n) because it needs to traverse the list, which takes O(n)
+
+- Deleting an element (Deleting itself is O(1)):
+
+    - Deleting at the front: O(1)
+
+    - anywhere else: O(n) because it needs to traverse the list, which takes O(n)
