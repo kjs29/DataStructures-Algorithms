cbc68de89dd53dab55ff7b034d85887e89f10b43
Jinsung <jsk.jinsung@gmail.com>
2023-08-10 23:49:45 -0600

    added 8.9 fib with dynamic programming

--- Technical Problems/leetcode.py
+++ Technical Problems/leetcode.py
@@ -11,6 +11,9 @@ leetcode_questions_solved[141] = "Linked List Cycle"
 leetcode_questions_solved[206] = "Reverse Linked List"
 leetcode_questions_solved[704] = "Binary Search"
 leetcode_questions_solved[11] = "Container with most water"
+leetcode_questions_solved[74] = "Search a 2D Matrix"
+leetcode_questions_solved[153] = "Find Minimum in Rotated Sorted Array"
+leetcode_questions_solved[941] = "Valid Mountain Array"
 
 
 
@@ -21,6 +24,8 @@ leetcode_questions_not_solved.update({15:"Three Sum"})
 leetcode_questions_not_solved.update({1290:"Convert Binary Number in a Linked List to Integer"})
 leetcode_questions_not_solved.update({21:"Merge Two Sorted Lists"})
 leetcode_questions_not_solved.update({34:"Find First and Last Position of Element in Sorted Array"})
+leetcode_questions_not_solved.update({1260:"Shift 2D Grid"})
+
 
 
 leetcode_questions_requires_more_practice = {}
@@ -31,6 +36,8 @@ leetcode_questions_requires_more_practice[88] = leetcode_questions_solved[88]
 leetcode_questions_requires_more_practice[707] = leetcode_questions_solved[707]
 leetcode_questions_requires_more_practice[876] = leetcode_questions_solved[876]
 leetcode_questions_requires_more_practice[704] = leetcode_questions_solved[704]
+leetcode_questions_requires_more_practice[153] = leetcode_questions_solved[153]
+leetcode_questions_requires_more_practice[941] = leetcode_questions_solved[941]
 
 print(f"\nleetcode_questions_solved : {leetcode_questions_solved}")
 print(f"\nleetcode_questions_not_solved : {leetcode_questions_not_solved}")
